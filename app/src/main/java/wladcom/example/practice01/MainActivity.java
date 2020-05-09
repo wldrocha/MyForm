8092f9fecae3542f6cc504117c5c9732f70a0b59
wladimir Rocha <wld.rocha@gmail.com>
2020-05-09 02:22:09 -0400

    First display, take a text

--- app/src/main/java/wladcom/example/practice01/MainActivity.java
+++ app/src/main/java/wladcom/example/practice01/MainActivity.java
@@ -2,13 +2,46 @@ package wladcom.example.practice01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //UI Elements
+    private EditText editTextName;
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //show logo, all versions android
+        getSupportActionBar().setLogo(R.mipmap.craneo_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Instance UI elements
+        editTextName = findViewById(R.id.editTextName);
+        btnNext = findViewById(R.id.buttonNextActivity);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString();
+                if(name != null && !name.isEmpty()){
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Please tiping your name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
